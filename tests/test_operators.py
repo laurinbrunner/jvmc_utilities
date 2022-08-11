fe3c11a22ac261ea10d879b18624da0a3bcb875f
laurinbrunner <laurin.brunner@physik.uni-augsburg.de>
2022-08-11 09:55:24 +0200

    extended test case

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -14,3 +14,12 @@ def test_higher_order_M_T_inv():
 
     assert(jnp.allclose(_M_2, M_2))
     assert(jnp.allclose(_T_2, T_2))
+
+    _M_3 = jnp.array([[[jnp.kron(jnp.kron(povm.M[i], povm.M[j]), povm.M[k]) for k in range(4)] for j in range(4)]
+                      for i in range(4)]).reshape(4**3, 2**3, 2**3)
+    _T_3 = jnp.kron(jnp.kron(povm.T_inv, povm.T_inv), povm.T_inv)
+
+    M_3, T_3 = higher_order_M_T_inv(3, povm.M, povm.T_inv)
+
+    assert(jnp.allclose(_M_3, M_3))
+    assert(jnp.allclose(_T_3, T_3))
