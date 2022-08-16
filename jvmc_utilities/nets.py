435a2cd72d584a4125e6fbdf5a85649a31448e79
laurinbrunner <laurin.brunner@physik.uni-augsburg.de>
2022-08-16 16:11:37 +0200

    added nets.py; still need implementation

---
+++ jvmc_utilities/nets.py
@@ -0,0 +1,11 @@
+import jax.numpy as jnp
+import jVMC
+from flax import linen as nn
+
+
+class POVMCNN(nn.Module):
+    """
+    Autoregressive implementation of a Convolutional Neural Network.
+    """
+    def __init__(self):
+        raise NotImplementedError()
