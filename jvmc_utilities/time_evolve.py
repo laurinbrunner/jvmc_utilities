844099b7b286578ae593b7aebe9dfcc1eb314354
laurinbrunner <laurin.brunner@physik.uni-augsburg.de>
2022-10-04 15:47:42 +0200

    added physical time resolved TDVP Error to writer

--- jvmc_utilities/time_evolve.py
+++ jvmc_utilities/time_evolve.py
@@ -379,6 +379,9 @@ class TimeEvolver:
 
         self.writer.write_scalars(self.write_index, {"dt": dt, "t": t, "tdvp_Error": tdvp_errs[0],
                                                      "tdvp_Residual": tdvp_errs[1]})
+        writedict["tdvp_Error/time"] = tdvp_errs[0]
+        writedict["tdvp_Residual/time"] = tdvp_errs[1]
+
         self.write_index += 1
         self.writer.write_scalars(jnp.floor(1E6*t), writedict)
 
