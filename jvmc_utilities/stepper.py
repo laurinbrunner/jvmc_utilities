242950817b5138c71b0a3473530e3380c2ef4910
laurinbrunner <laurin.brunner@physik.uni-augsburg.de>
2023-01-09 16:34:54 +0100

    added stepper.py

---
+++ jvmc_utilities/stepper.py
@@ -0,0 +1,10 @@
+import numpy as np
+import jax.numpy as jnp
+
+
+class BulirschStoer:
+    """
+    This class implements the Bulirsch-Stoer method as described in 'Numerical Recipes: The Art of Scientific Computing,
+    Third Edition' Chapter 17.3.
+    """
+    pass
