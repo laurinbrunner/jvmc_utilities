712ad7106d7fc5a434488bc63208523a55ecb11f
laurinbrunner <laurin.brunner@physik.uni-augsburg.de>
2023-07-10 17:25:21 +0200

    added further tests

--- tests/test_nets.py
+++ tests/test_nets.py
@@ -36,6 +36,96 @@ def test_POVMCNN():
 
 
 @pytest.mark.slow
+def test_POVMCNNResidual():
+    L = 4
+
+    cnn = jvmc_utilities.nets.POVMCNNResidual(L=L, depth=2, features=4)
+
+    psi = jVMC.vqs.NQS(cnn, seed=1234)
+
+    sampler = jVMC.sampler.ExactSampler(psi, (L,), lDim=4, logProbFactor=1)
+
+    tdvpEquation = jVMC.util.tdvp.TDVP(sampler, rhsPrefactor=-1., pinvTol=1e-6, diagonalShift=0, makeReal='real',
+                                       crossValidation=False)
+
+    stepper = jVMC.util.stepper.Euler(timeStep=1e-2)
+
+    povm = jVMC.operator.POVM({"dim": "1D", "L": L})
+    lind = jVMC.operator.POVMOperator(povm)
+    jvmc_utilities.operators.initialisation_operators(povm)
+    lind.add({"name": "updown_dis", "strength": 5.0, "sites": (0, 1)})
+    lind.add({"name": "updown_dis", "strength": 5.0, "sites": (2, 3)})
+
+    measurer = jvmc_utilities.measurement.Measurement(sampler, povm)
+    measurer.set_observables(["Sz_i"])
+    init = jvmc_utilities.time_evolve.Initializer(psi, tdvpEquation, stepper, lind, measurer=measurer)
+
+    init.initialize(measure_step=-1, steps=100)
+
+    assert jnp.allclose(measurer.measure()["Sz_i"], jnp.array([1., -1., 1., -1.]), atol=0.01)
+
+
+@pytest.mark.slow
+def test_CNNAttention():
+    L = 4
+
+    cnn = jvmc_utilities.nets.CNNAttention(L=L, depth=2, features=4)
+
+    psi = jVMC.vqs.NQS(cnn, seed=1234)
+
+    sampler = jVMC.sampler.ExactSampler(psi, (L,), lDim=4, logProbFactor=1)
+
+    tdvpEquation = jVMC.util.tdvp.TDVP(sampler, rhsPrefactor=-1., pinvTol=1e-6, diagonalShift=0, makeReal='real',
+                                       crossValidation=False)
+
+    stepper = jVMC.util.stepper.Euler(timeStep=1e-2)
+
+    povm = jVMC.operator.POVM({"dim": "1D", "L": L})
+    lind = jVMC.operator.POVMOperator(povm)
+    jvmc_utilities.operators.initialisation_operators(povm)
+    lind.add({"name": "updown_dis", "strength": 5.0, "sites": (0, 1)})
+    lind.add({"name": "updown_dis", "strength": 5.0, "sites": (2, 3)})
+
+    measurer = jvmc_utilities.measurement.Measurement(sampler, povm)
+    measurer.set_observables(["Sz_i"])
+    init = jvmc_utilities.time_evolve.Initializer(psi, tdvpEquation, stepper, lind, measurer=measurer)
+
+    init.initialize(measure_step=-1, steps=100)
+
+    assert jnp.allclose(measurer.measure()["Sz_i"], jnp.array([1., -1., 1., -1.]), atol=0.01)
+
+
+@pytest.mark.slow
+def test_CNNAttentionResidual():
+    L = 4
+
+    cnn = jvmc_utilities.nets.CNNAttentionResidual(L=L, depth=2, features=4)
+
+    psi = jVMC.vqs.NQS(cnn, seed=1234)
+
+    sampler = jVMC.sampler.ExactSampler(psi, (L,), lDim=4, logProbFactor=1)
+
+    tdvpEquation = jVMC.util.tdvp.TDVP(sampler, rhsPrefactor=-1., pinvTol=1e-6, diagonalShift=0, makeReal='real',
+                                       crossValidation=False)
+
+    stepper = jVMC.util.stepper.Euler(timeStep=1e-2)
+
+    povm = jVMC.operator.POVM({"dim": "1D", "L": L})
+    lind = jVMC.operator.POVMOperator(povm)
+    jvmc_utilities.operators.initialisation_operators(povm)
+    lind.add({"name": "updown_dis", "strength": 5.0, "sites": (0, 1)})
+    lind.add({"name": "updown_dis", "strength": 5.0, "sites": (2, 3)})
+
+    measurer = jvmc_utilities.measurement.Measurement(sampler, povm)
+    measurer.set_observables(["Sz_i"])
+    init = jvmc_utilities.time_evolve.Initializer(psi, tdvpEquation, stepper, lind, measurer=measurer)
+
+    init.initialize(measure_step=-1, steps=100)
+
+    assert jnp.allclose(measurer.measure()["Sz_i"], jnp.array([1., -1., 1., -1.]), atol=0.03)
+
+
+@pytest.mark.slow
 def test_POVMCNNGated():
     L = 4
 
@@ -154,6 +244,69 @@ def test_symmetric_POVMCNN():
     orbit = jVMC.util.symmetries.get_orbit_1D(L, "translation")
    
     cnn = jvmc_utilities.nets.POVMCNN(L=L, orbit=orbit)
+
+    psi = jVMC.vqs.NQS(cnn, seed=1234)
+    sampler = jVMC.sampler.ExactSampler(psi, (L,), lDim=4, logProbFactor=1)
+
+    configs = jnp.array([[[1, 0, 0, 0],
+                          [0, 1, 0, 0],
+                          [0, 0, 1, 0],
+                          [0, 0, 0, 1]]])
+
+    logprobs = psi(configs)
+
+    assert jnp.unique(jnp.round(logprobs, 12)).shape[0] == 1
+
+
+@pytest.mark.slow
+def test_symmetric_POVMCNNResidual():
+    L = 4
+
+    orbit = jVMC.util.symmetries.get_orbit_1D(L, "translation")
+
+    cnn = jvmc_utilities.nets.POVMCNNResidual(L=L, orbit=orbit)
+
+    psi = jVMC.vqs.NQS(cnn, seed=1234)
+    sampler = jVMC.sampler.ExactSampler(psi, (L,), lDim=4, logProbFactor=1)
+
+    configs = jnp.array([[[1, 0, 0, 0],
+                          [0, 1, 0, 0],
+                          [0, 0, 1, 0],
+                          [0, 0, 0, 1]]])
+
+    logprobs = psi(configs)
+
+    assert jnp.unique(jnp.round(logprobs, 12)).shape[0] == 1
+
+
+@pytest.mark.slow
+def test_symmetric_CNNAttention():
+    L = 4
+
+    orbit = jVMC.util.symmetries.get_orbit_1D(L, "translation")
+
+    cnn = jvmc_utilities.nets.CNNAttention(L=L, orbit=orbit)
+
+    psi = jVMC.vqs.NQS(cnn, seed=1234)
+    sampler = jVMC.sampler.ExactSampler(psi, (L,), lDim=4, logProbFactor=1)
+
+    configs = jnp.array([[[1, 0, 0, 0],
+                          [0, 1, 0, 0],
+                          [0, 0, 1, 0],
+                          [0, 0, 0, 1]]])
+
+    logprobs = psi(configs)
+
+    assert jnp.unique(jnp.round(logprobs, 12)).shape[0] == 1
+
+
+@pytest.mark.slow
+def test_symmetric_CNNAttentionResidual():
+    L = 4
+
+    orbit = jVMC.util.symmetries.get_orbit_1D(L, "translation")
+
+    cnn = jvmc_utilities.nets.CNNAttentionResidual(L=L, orbit=orbit)
 
     psi = jVMC.vqs.NQS(cnn, seed=1234)
     sampler = jVMC.sampler.ExactSampler(psi, (L,), lDim=4, logProbFactor=1)
